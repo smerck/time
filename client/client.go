39632c80edab84453d9f224bff1cb1a8972dbd4a
smerck <43017340+smerck@users.noreply.github.com>
2020-07-27 00:35:27 -0700

    Cleanup (#3)
    * cleaning up client and server code

--- client/client.go
+++ client/client.go
@@ -10,13 +10,12 @@ import (
 )
 
 func main() {
-	var rate float64
+	var rate int64
 	var url string
-	var testDuration time.Duration
-	var count, failed int
-	parseFlags(&rate, &url, &testDuration)
+	var duration, count, failed int
+	parseFlags(&rate, &url, &duration)
 
-	tickRate := time.Duration(1/rate*1000) * time.Millisecond
+	tickRate := time.Duration(1/float64(rate)*1000) * time.Millisecond
 	ticker := time.NewTicker(tickRate)
 	done := make(chan bool)
 	c := http.Client{Timeout: time.Second * 2}
@@ -46,7 +45,7 @@ func main() {
 		}
 	}()
 
-	time.Sleep(testDuration)
+	time.Sleep(time.Duration(duration) * time.Second)
 	ticker.Stop()
 	done <- true
 	fmt.Println("Test complete")
@@ -54,16 +53,19 @@ func main() {
 	log.Printf("Overall Success Rate: %.2f%%", sr)
 }
 
-func parseFlags(rate *float64, url *string, duration *time.Duration) {
-	flag.Float64Var(rate, "rps", 5, "number of requests that can be sent per second.")
+func parseFlags(rate *int64, url *string, duration *time.Duration) {
+	flag.Int64Var(rate, "rps", 5, "number of requests that can be sent per second.")
 	flag.StringVar(url, "host", "http://localhost:9001", "hostname, including http[s]://")
 	flag.DurationVar(duration, "duration", time.Duration(120)*time.Second, "duration of test")
 	flag.Parse()
 	if !strings.HasPrefix(*url, "http://") {
-		log.Fatal("Specify protocol in hostname")
+		log.Fatal("Specify protocol in hostname.")
 	}
 	if *rate > 200 || *rate < 1 {
-		log.Fatal("Rate must be between 1 and 100")
+		log.Fatal("Rate must be between 1 and 100.")
 	}
-	log.Printf("Starting test: Sending %.0f rps to %s for %v", *rate, *url, duration)
+	if *duration > 0 {
+		log.Fatal("Duration must be greater than 0.")
+	}
+	log.Printf("Starting test: Sending %d requests per second to %s for %vs", *rate, *url, duration.Seconds())
 }
