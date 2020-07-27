14104b8666a5601874b24cdb338c2489596da50a
Stephan Mercatoris <smercatoris@example.com>
2020-07-27 04:59:44 +0000

    adding client

--- client/client.go
+++ client/client.go
@@ -1,64 +1,69 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
 func main() {
-	//parse flags
-	rate := 400
+	var rate float64
+	var url string
+	var testDuration time.Duration
+	var count, failed int
+	parseFlags(&rate, &url, &testDuration)
 
-	ticker := time.NewTicker(time.Second / rate)
+	tickRate := time.Duration(1/rate*1000) * time.Millisecond
+	ticker := time.NewTicker(tickRate)
 	done := make(chan bool)
-
 	c := http.Client{Timeout: time.Second * 2}
 
-	 respChan := make(chan http.Response)
-	// send request according to rate
 	go func() {
 		for {
 			select {
 			case <-done:
-				return nil
+				return
 			case <-ticker.C:
-				success := 0
-				//var latency time.Duration
-				//log.Printf("Sending %d requests\n", rate)
-				//for i := 0; i < rate; i++ {
 				start := time.Now()
-				go c.Get("http://localhost:9001")
+				resp, err := c.Get(url)
 				if err != nil {
-					log.Fatal("Server Error: %s", err)
+					log.Fatalf("Server Error: %s", err)
 				}
 				defer resp.Body.Close()
 				d := time.Since(start)
-				if resp.StatusCode == 200 {
-					success++
+				status := "succeeded"
+				if resp.StatusCode != 200 {
+					status = "failed"
+					failed++
 				}
-				latency += d
+				count++
+				log.Printf("Request %s with Status Code %d after %v\n", status, resp.StatusCode, d)
+
 			}
 		}
 	}()
 
-    go func() {
-		select { 
-		case <-success:
-			success++
-		}
-		case <-fail:
-			
-	}
-	
-	
+	time.Sleep(testDuration)
 	ticker.Stop()
 	done <- true
-	fmt.Println("Test stopped")
-
+	fmt.Println("Test complete")
+	sr := float64((count-failed)/count) * 100
+	log.Printf("Overall Success Rate: %.2f%%", sr)
 }
 
-func fetch(url, ch chan<- http.Response) {
-
+func parseFlags(rate *float64, url *string, duration *time.Duration) {
+	flag.Float64Var(rate, "rps", 5, "number of requests that can be sent per second.")
+	flag.StringVar(url, "host", "http://localhost:9001", "hostname, including http[s]://")
+	flag.DurationVar(duration, "duration", time.Duration(120)*time.Second, "duration of test")
+	flag.Parse()
+	if !strings.HasPrefix(*url, "http://") {
+		log.Fatal("Specify protocol in hostname")
+	}
+	if *rate > 200 || *rate < 1 {
+		log.Fatal("Rate must be between 1 and 100")
+	}
+	log.Printf("Starting test: Sending %.0f rps to %s for %v", *rate, *url, duration)
 }
